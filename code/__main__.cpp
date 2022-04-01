c476ed5097460d8e25717d1c68c78a48558cc6a8
Pierre Boistier <services.boistier@protonmail.com>
2022-04-01 10:00:36 +0200

    séparation main et fonctions joueur humain

--- code/__main__.cpp
+++ code/__main__.cpp
@@ -1,63 +1,27 @@
 #include <iostream>     // entree/sorties
 #include <math.h>
 
-
 using namespace std ;
-
-#include "annexes.cpp"
-#include "grille.cpp"
 
 // déclaration des variables globales
 
 int noir = 22 ;
 int blanc = 11 ;
 
+// inclusion des classes et fonctions
+
+#include "annexes.cpp"			// fonctions
+#include "grille.cpp"				// classe
+#include "jouer.cpp"				// fonctions
+
 // éxecution du programme final en C++
-
-bool jeuFini()
-{
-	for (size_t i = 0 ; i < 8 ; i++)
-		for (size_t j = 0 ; j < 8 ; j++)
-			if (liciteB(grille[i][j]) || liciteN(grille[i][j]) || estVide(grille[i][j]))
-				return false ;
-	return true ;
-}
-
-void demanderCoups(int couleur, int* coup_x, int* coup_y)		// On utlise des pointeurs car le c++ ne peut pas renvoyer de couple...
-{
-	if (couleur == blanc)
-		cout << "Au tour de blanc: " << endl ;
-	else
-		cout << "Au tour de noir: " << endl ;
-	cout <<  "Entrer la position en x (ligne): " ;
-	cin >> *coup_x ;
-	cout <<  "Entrer la position en y (colonne): " ;
-	cin >> *coup_y ;
-}
-
-int jouer()			// humain vs humain pour l'instant
-{
-	int couleur = noir ;
-	int coup_x = 0, coup_y = 0 ;
-	affichageJeu() ;
-	while (!jeuFini())
-	{
-		demanderCoups(couleur, &coup_x, &coup_y) ;
-		retournerPlacer(coup_x, coup_y, couleur) ;
-		affichageJeu() ;
-		couleur = changeCouleur(couleur) ;
-	}
-}
-
 
 int main()
 {
-	grille my_grille ;
-	my_grille.init() ;
-	my_grille.affichageJeu() ;
-	my_grille.retournerPlacer(5,4,22) ;
-	my_grille.affichageJeu() ;
-	my_grille.affichageBrut() ;
+	grille grille_de_jeu ;
+	grille_de_jeu.init() ;
+
+	jouer(grille_de_jeu) ;
 
 	return 0 ;
 }
